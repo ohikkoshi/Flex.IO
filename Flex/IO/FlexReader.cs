8168b76c34cc77308b0882847c1a3157893776fc
ohikkoshi <ohikkoshi@icloud.com>
2019-11-06 14:28:51 +0900

    add code.

---
+++ Flex/IO/FlexReader.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Flex.IO
+{
+	public class FlexReader : IDisposable
+	{
+		MemoryStream stream;
+
+
+		public FlexReader(byte[] buffer) => stream = new MemoryStream(buffer);
+
+		FlexReader()
+		{
+		}
+
+		~FlexReader()
+		{
+			Dispose();
+		}
+
+		public void Dispose()
+		{
+			if (stream != null) {
+				stream.Close();
+				stream = null;
+			}
+		}
+
+		public bool TryRead(int length, out byte[] data)
+		{
+			Debug.Assert(stream != null);
+
+			data = new byte[length];
+
+			return (stream.Read(data, 0, length) == length);
+		}
+
+		public byte Byte()
+		{
+			if (TryRead(sizeof(byte), out byte[] data)) {
+				return data[0];
+			}
+
+			return 0;
+		}
+
+		public short Short()
+		{
+			if (TryRead(sizeof(short), out byte[] data)) {
+				return BitConverter.ToInt16(data, 0);
+			}
+
+			return 0;
+		}
+
+		public int Int()
+		{
+			if (TryRead(sizeof(int), out byte[] data)) {
+				return BitConverter.ToInt32(data, 0);
+			}
+
+			return 0;
+		}
+
+		public long Long()
+		{
+			if (TryRead(sizeof(long), out byte[] data)) {
+				return BitConverter.ToInt64(data, 0);
+			}
+
+			return 0;
+		}
+
+		public float Float()
+		{
+			if (TryRead(sizeof(float), out byte[] data)) {
+				return BitConverter.ToSingle(data, 0);
+			}
+
+			return 0;
+		}
+
+		public double Double()
+		{
+			if (TryRead(sizeof(double), out byte[] data)) {
+				return BitConverter.ToDouble(data, 0);
+			}
+
+			return 0;
+		}
+
+		public string String()
+		{
+			int length = Int();
+
+			if (TryRead(length, out byte[] data)) {
+				return Encoding.UTF8.GetString(data);
+			}
+
+			return null;
+		}
+	}
+}
---
+++ Flex/IO/FlexWriter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Flex.IO
+{
+	public class FlexWriter : IDisposable
+	{
+		MemoryStream stream;
+
+
+		public FlexWriter() => stream = new MemoryStream();
+		public FlexWriter(byte[] buffer) => stream = new MemoryStream(buffer);
+		public FlexWriter(int capacity) => stream = new MemoryStream(capacity);
+
+		~FlexWriter()
+		{
+			Dispose();
+		}
+
+		public void Dispose()
+		{
+			if (stream != null) {
+				stream.Close();
+				stream = null;
+			}
+		}
+
+		public void Reset()
+		{
+			Debug.Assert(stream != null);
+
+			stream.Position = 0L;
+			stream.SetLength(0L);
+		}
+
+		public byte[] Flush()
+		{
+			Debug.Assert(stream != null);
+
+			if (stream.TryGetBuffer(out ArraySegment<byte> buffer)) {
+				Reset();
+				return buffer.Array;
+			}
+
+			return null;
+		}
+
+		public void Write(byte[] bytes)
+		{
+			stream?.Write(bytes, 0, bytes.Length);
+		}
+
+		public void Write(byte value)
+		{
+			stream?.WriteByte(value);
+		}
+
+		public void Write(short value)
+		{
+			Write(BitConverter.GetBytes(value));
+		}
+
+		public void Write(int value)
+		{
+			Write(BitConverter.GetBytes(value));
+		}
+
+		public void Write(long value)
+		{
+			Write(BitConverter.GetBytes(value));
+		}
+
+		public void Write(float value)
+		{
+			Write(BitConverter.GetBytes(value));
+		}
+
+		public void Write(double value)
+		{
+			Write(BitConverter.GetBytes(value));
+		}
+
+		public void Write(string value)
+		{
+			byte[] buf = Encoding.UTF8.GetBytes(value);
+			Write(buf.Length);
+			Write(buf);
+		}
+	}
+}
